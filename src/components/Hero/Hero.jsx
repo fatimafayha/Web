1d5bd42ae343aab8e4970de91ca1f95e3c054e10
Fatima Fayha <fatimafayha@gmail.com>
2024-05-08 18:09:27 -0400

    Update Hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -11,9 +11,6 @@ export const Hero = () => {
         <p className={styles.description}>
          I am a computer Science grad with interest in Front-End, Back-end and Machine Learning. 
         </p>
-        <a href="https://docs.google.com/document/d/1cjZ015TYP6OsTk_3VilH7wfBc4Fzzf6-UPMrUmX73hw/edit?usp=drive_link" target="_blank" className={styles.resumeBtn}>
-          See My Resume
-        </a>
       </div>
       <img
         src={getImageUrl("hero/heroImage.png")}
